1dd462df8ea113c059f7dd3e3186fdc917e22239
Lorenzo <lorenzo@Lorenzos-MacBook-Air.local>
2025-12-08 22:55:10 -0300

    boto3

--- src/lomnia_ingester/__main__.py
+++ src/lomnia_ingester/__main__.py
@@ -1,18 +1,19 @@
+import boto3
+from dotenv import load_dotenv
+from mypy_boto3_s3 import S3Client
+
+load_dotenv()
+
+
 def foo(bar: str) -> str:
-    """Summary line.
-
-    Extended description of function.
-
-    Args:
-        bar: Description of input argument.
-
-    Returns:
-        Description of return value
-    """
-
+    s3: S3Client = boto3.client(
+        "s3",
+        endpoint_url="http://localhost:3900",
+    )
+    print(s3.list_objects(Bucket="lomnia-raw"))
     return bar
 
 
 if __name__ == "__main__":
-    print("Test")
+    foo("Test")
     pass
