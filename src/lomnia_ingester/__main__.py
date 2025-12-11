073e557426473c0d08e9aee038ca8fcb06c5617d
Lorenzo <lorenzopicoli@me.com>
2025-12-10 22:35:13 -0300

    playing with idea of pulling plugins and running them

--- src/lomnia_ingester/__main__.py
+++ src/lomnia_ingester/__main__.py
@@ -1,3 +1,9 @@
+import os
+import shutil
+import subprocess
+import tempfile
+from pathlib import Path
+
 import boto3
 import pika
 from dotenv import load_dotenv
@@ -35,7 +41,42 @@ def send_message(message: str, queue_name: str = "test_queue"):
     conn.close()
 
 
+def run_plugin(repo_url: str, folder: str):
+    tmp = Path(tempfile.mkdtemp())
+    plugin_dir = tmp / "plugin"
+    work_dir = plugin_dir / folder
+    try:
+        subprocess.run(["git", "clone", repo_url, str(plugin_dir)], check=True)  # noqa: S603, S607
+
+        subprocess.run(["uv", "sync"], cwd=work_dir, check=True)  # noqa: S607
+        env = {
+            **os.environ,  # keep existing env vars
+            "OWNTRACKS_USER": "owntracks",
+            "OWNTRACKS_DEVICE": "shiba",
+        }
+        result = subprocess.run(
+            ["uv", "run", "extract", "--start_date", "1765058675", "--out_dir", ""],  # noqa: S607
+            env=env,
+            cwd=work_dir,
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        print("OUTPUT:", result.stdout)
+        print("STDERR:", result.stderr)
+    except subprocess.CalledProcessError as e:
+        print("----- SUBPROCESS ERROR -----")
+        print("COMMAND:", e.cmd)
+        print("RETURN CODE:", e.returncode)
+        print("STDOUT:", e.stdout)
+        print("STDERR:", e.stderr)
+        raise
+    finally:
+        shutil.rmtree(tmp, ignore_errors=True)
+
+
 if __name__ == "__main__":
-    garage_buckets()
-    send_message("Hello world")
+    # garage_buckets()
+    # send_message("Hello world")
+    run_plugin("https://github.com/lorenzopicoli/lomnia-plugins.git", folder="owntracks-recorder")
     pass
